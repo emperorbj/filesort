dccb7602b0038b0b6b6638facbc79b2bd7b47df3
emperorbj <cwaku96@gmail.com>
2024-06-07 09:24:36 +0100

    cleared previous code

--- script.py
+++ script.py
@@ -1,14 +1,4 @@
-# import os
 
-# file_path = "C:/Users/USER/Pictures/error1.png"
-# print(file_path)
-
-# if os.path.isfile(file_path):
-#     os.remove(file_path)
-#     print("the file exists and has been deleted")
-
-# else:
-#     print("the file does not exist")
 import os
 
 def delete_files_by_extension(directory, extensions):
